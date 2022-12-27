ef451ca70c697c08c448cd4ee4c9ba735dfc253a
Ryncler <2453580912@qq.com>
2022-12-27 18:41:32 +0800

    添加base服务构建管道，修复找不到dockerfile的问题

---
+++ host/BaseService/host/BaseService.HttpApi.Host/handle.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ -n $(docker ps -q -f "name=^te-base-service$") ]];then
+	docker stop te-base-service
+    docker rm te-base-service
+else
+	echo "te-base-service not exist"
+fi
+
+CON=`docker image ls 'base-service' | wc -l` 
+if [ $CON -eq 2 ] 
+then
+docker images | grep base-service | awk '{print $3}' | xargs docker rmi -f
+fi
+
+docker build -t base-service:latest .
+
+docker tag base-service 192.168.110.101:90/travel-enjoyment/base-service:latest
+
+docker push 192.168.110.101:90/travel-enjoyment/base-service:latest
+
+docker run -d --name te-base-service -p 53300:53300 base-service
