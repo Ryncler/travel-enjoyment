ae086f5086a2db781ce4e6331a12178179014c88
Ryncler <2453580912@qq.com>
2023-01-08 21:42:06 +0800

    修复配置文件不更新的问题

--- host/BaseService/host/BaseService.Host/BaseServiceHostModule.cs
+++ host/BaseService/host/BaseService.Host/BaseServiceHostModule.cs
@@ -51,7 +51,6 @@ public class BaseServiceHostModule : AbpModule
             options.UseNpgsql();
         });
 
-
         Configure<AbpMultiTenancyOptions>(options =>
         {
             options.IsEnabled = MultiTenancyConsts.IsEnabled;
