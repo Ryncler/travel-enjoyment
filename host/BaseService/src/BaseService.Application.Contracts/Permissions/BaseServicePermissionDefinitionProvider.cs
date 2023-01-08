c44059afc38b03f4f382afeb5014774674124d54
Ryncler <2453580912@qq.com>
2023-01-08 21:09:01 +0800

    修改默认语言为中文，添加对应权限

--- host/BaseService/src/BaseService.Application.Contracts/Permissions/BaseServicePermissionDefinitionProvider.cs
+++ host/BaseService/src/BaseService.Application.Contracts/Permissions/BaseServicePermissionDefinitionProvider.cs
@@ -8,12 +8,25 @@ public class BaseServicePermissionDefinitionProvider : PermissionDefinitionProvi
 {
     public override void Define(IPermissionDefinitionContext context)
     {
-        var myGroup = context.AddGroup(BaseServicePermissions.GroupName, L("Permission:BaseService"));
+        var myGroup = context.AddGroup(BaseServicePermissions.GroupName, L("基础服务管理"));
 
-        var userExtensionPermission = myGroup.AddPermission(BaseServicePermissions.UserExtension.Default, L("Permission:UserExtension"));
-        userExtensionPermission.AddChild(BaseServicePermissions.UserExtension.Create, L("Permission:Create"));
-        userExtensionPermission.AddChild(BaseServicePermissions.UserExtension.Update, L("Permission:Update"));
-        userExtensionPermission.AddChild(BaseServicePermissions.UserExtension.Delete, L("Permission:Delete"));
+        var userExtensionPermission = myGroup.AddPermission(BaseServicePermissions.UserExtension.Default, L("用户扩展管理"));
+        userExtensionPermission.AddChild(BaseServicePermissions.UserExtension.Create, L("创建"));
+        userExtensionPermission.AddChild(BaseServicePermissions.UserExtension.Update, L("更新"));
+        userExtensionPermission.AddChild(BaseServicePermissions.UserExtension.Delete, L("删除"));
+
+        var openIddictPermission = myGroup.AddPermission(BaseServicePermissions.OpenIddict.Default, L("OpenIddict管理"));
+        openIddictPermission.AddChild(BaseServicePermissions.OpenIddict.CreateApplication, L("创建应用"));
+        openIddictPermission.AddChild(BaseServicePermissions.OpenIddict.UpdateApplication, L("更新应用"));
+        openIddictPermission.AddChild(BaseServicePermissions.OpenIddict.DeleteApplication, L("删除应用"));
+        openIddictPermission.AddChild(BaseServicePermissions.OpenIddict.CreateScope, L("创建服务"));
+        openIddictPermission.AddChild(BaseServicePermissions.OpenIddict.UpdateScope, L("更新服务"));
+        openIddictPermission.AddChild(BaseServicePermissions.OpenIddict.DeleteScope, L("删除服务"));
+
+        var userManage = myGroup.AddPermission(BaseServicePermissions.UserManage.Default, L("用户管理"));
+        userManage.AddChild(BaseServicePermissions.UserManage.Create, L("创建"));
+        userManage.AddChild(BaseServicePermissions.UserManage.Update, L("更新"));
+        userManage.AddChild(BaseServicePermissions.UserManage.Delete, L("删除"));
     }
 
     private static LocalizableString L(string name)
--- host/BaseService/src/BaseService.Application.Contracts/Permissions/BaseServicePermissions.cs
+++ host/BaseService/src/BaseService.Application.Contracts/Permissions/BaseServicePermissions.cs
@@ -18,4 +18,24 @@ public class BaseServicePermissions
         public const string Create = Default + ".Create";
         public const string Delete = Default + ".Delete";
     }
+
+    public class OpenIddict
+    {
+        public const string Default = GroupName + ".OpenIddict";
+        public const string UpdateScope = Default + ".UpdateScope";
+        public const string CreateScope = Default + ".CreateScope";
+        public const string DeleteScope = Default + ".DeleteScope";
+
+        public const string UpdateApplication = Default + ".UpdateApplication";
+        public const string CreateApplication = Default + ".CreateApplication";
+        public const string DeleteApplication = Default + ".DeleteApplication";
+    }
+
+    public class UserManage
+    {
+        public const string Default = GroupName + ".UserManage";
+        public const string Update = Default + ".Update";
+        public const string Create = Default + ".Create";
+        public const string Delete = Default + ".Delete";
+    }
 }
