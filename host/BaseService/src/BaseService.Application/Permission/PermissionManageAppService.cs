806313140776cc96defea5f57c9fc256e0cbfe09
Ryncler <2453580912@qq.com>
2023-01-28 23:09:43 +0800

    base服务添加把所有权限添加至admin的接口

---
+++ host/BaseService/src/BaseService.Application/Permission/PermissionManageAppService.cs
@@ -0,0 +1,38 @@
+﻿using BaseService.Permissions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Authorization;
+using Volo.Abp.Authorization.Permissions;
+using Volo.Abp.PermissionManagement;
+
+namespace BaseService.PermissionManage
+{
+    public class PermissionManageAppService : BaseServiceAppService, IPermissionManageAppService
+    {
+        private readonly IPermissionManager _permissionManager;
+
+        public PermissionManageAppService(IPermissionManager permissionManager)
+        {
+            _permissionManager = permissionManager;
+        }
+
+        [Authorize]
+        public async Task AddAllPermissionToAdmin()
+        {
+            if (!CurrentUser.IsInRole("admin"))
+                throw new AbpAuthorizationException();
+
+            var permissionList = await _permissionManager.GetAllAsync("R", "admin");
+            permissionList.RemoveAll(x => x.Name.Equals("AbpIdentity.UserLookup"));
+            foreach (var item in permissionList)
+            {
+                await _permissionManager.SetForRoleAsync("admin", item.Name, true);
+            }
+        }
+    }
+}
---
+++ host/BaseService/src/BaseService.HttpApi/Entities/PermissionManageController.cs
@@ -0,0 +1,31 @@
+﻿using BaseService.Permissions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Volo.Abp;
+
+namespace BaseService.Entities
+{
+    [Area(BaseServiceRemoteServiceConsts.ModuleName)]
+    [RemoteService(Name = BaseServiceRemoteServiceConsts.RemoteServiceName)]
+    [Route("api/permission-manage")]
+    public class PermissionManageController : BaseServiceController, IPermissionManageAppService
+    {
+        private readonly IPermissionManageAppService _permissionManageAppService;
+        public PermissionManageController(IPermissionManageAppService permissionManageAppService)
+        {
+            _permissionManageAppService = permissionManageAppService;
+        }
+
+        [HttpGet]
+        [Route("all-to-admin")]
+        public Task AddAllPermissionToAdmin()
+        {
+            return _permissionManageAppService.AddAllPermissionToAdmin();
+        }
+    }
+}
