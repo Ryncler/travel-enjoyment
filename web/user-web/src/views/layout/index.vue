7f3ceae495adcf371b326d9c70e0708c467f52af
Ryncler <2453580912@qq.com>
2023-02-26 23:56:11 +0800

    项目：前端前台----提交说明：修复页脚不在最下方的问题

--- web/user-web/src/views/layout/index.vue
+++ web/user-web/src/views/layout/index.vue
@@ -1,26 +1,48 @@
 <template>
-    <div style=" flex-direction: column;height: 100%;">
-        <el-container>
-            <el-header class="header">
-                <headerVue></headerVue>
-            </el-header>
-            <el-main class="common-layout">
-                <mainVue></mainVue>
-            </el-main>
-            <el-footer class="common-layout" height="230px">
-                <footerVue></footerVue>
-            </el-footer>
-        </el-container>
-    </div>
+    <el-container id="contain">
+        <el-header class="header">
+            <headerVue></headerVue>
+        </el-header>
+        <el-main class="common-layout">
+            <mainVue></mainVue>
+        </el-main>
+        <el-footer id="footer" class="common-layout" height="230px">
+            <footerVue></footerVue>
+        </el-footer>
+    </el-container>
 </template>
 
 <script setup>
 import { ref } from 'vue';
-import { onBeforeMount } from '@vue/runtime-core';
+import { onBeforeMount, onMounted } from '@vue/runtime-core';
+import elementResizeDetectorMaker from 'element-resize-detector';
 import footerVue from '@/components/footer.vue';
 import headerVue from '@/components/layout/header.vue';
 import mainVue from '@/components/layout/main.vue'
 
+const setBottom = () => {
+    var visibleHight = document.documentElement.clientHeight
+    var containHight = document.getElementById('contain')
+    var footer = document.getElementById('footer')
+    if (containHight.offsetHeight < visibleHight) {
+        footer.style.marginTop = "482px";
+    }
+}
+const listen = () => {
+    const detector = elementResizeDetectorMaker()
+    detector.listenTo(document.getElementById('contain'), (item) => {
+        var footer = document.getElementById('footer')
+        if (footer.offsetTop > 2000) {
+            footer.style.marginTop = "80px";
+        }
+        if (footer.offsetTop < 400) {
+            footer.style.marginTop = "482px";
+        }
+    })
+}
+onMounted(() => {
+    listen(), setBottom()
+})
 </script>
 
 <style scoped>
@@ -37,12 +59,9 @@ import mainVue from '@/components/layout/main.vue'
 }
 
 .header {
-    flex: 0 0 auto;
-    display: flex;
     width: 100%;
     height: 100px;
 }
-
 </style>
 
 <style></style>
